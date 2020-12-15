273707ef56068193bae7899bddaf723692741029
yasu307 <yusuke307.ac@gmail.com>
2020-12-15 17:33:22 +0900

    add DummyData

---
+++ app/src/main/java/com/example/aona2/studywithme/DummyData.kt
@@ -0,0 +1,11 @@
+package com.example.aona2.studywithme
+
+class DummyData {
+    lateinit var user_id: String
+    lateinit var user_name: String
+    lateinit var user_icon: uri
+    lateinit var studing_task: String
+    lateinit var start_study_at:
+
+    constructor()
+}
