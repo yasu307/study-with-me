1eb84b32fcb97b391dcc7a93d980c744d71c1048
yasu307 <yusuke307.ac@gmail.com>
2020-12-16 11:53:16 +0900

    add TaskNameInputActivity

---
+++ app/src/main/java/com/example/aona2/studywithme/TaskNameInputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aona2.studywithme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskNameInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_name_input)
+    }
+}
