abf2f9d05e5bb655d9896fc633e61d0266c8bba1
yasu307 <yusuke307.ac@gmail.com>
2020-12-16 11:52:29 +0900

    add recyclerview to HomeActivity

--- app/src/main/java/com/example/aona2/studywithme/HomeActivity.kt
+++ app/src/main/java/com/example/aona2/studywithme/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.aona2.studywithme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val adapter = StudyingFriendListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/aona2/studywithme/StudingFriendListAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.aona2.studywithme
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ListMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_item.view.*
+
+class StudyingFriendListAdapter internal constructor(context: Context)
+    : RecyclerView.Adapter<StudyingFriendListAdapter.StudyingFriendViewHolder>(){
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+//    private val friends = emptyList<Friend>()
+
+    inner class StudyingFriendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val user_icon = itemView.user_icon
+        val user_name = itemView.user_name
+        val task_name = itemView.task_name
+        val remain_time = itemView.remain_time
+        val task_status_icon = itemView.task_status_icon
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyingFriendViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return StudyingFriendViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: StudyingFriendViewHolder, position: Int) {
+        if(position == 0){
+            holder.user_icon.setImageResource(R.drawable.member1)
+            holder.user_name.text = "ヨシダ"
+            holder.task_status_icon.setImageResource(R.drawable.study_status)
+            holder.remain_time.text = "残り15分"
+            holder.task_name.text = "実システム創造　進捗作成"
+        }
+        if(position == 1){
+            holder.user_icon.setImageResource(R.drawable.member2)
+            holder.user_name.text = "フクダ"
+            holder.task_status_icon.setImageResource(R.drawable.breaktime_status)
+            holder.remain_time.text = "残り2分"
+            holder.task_name.text = ""
+        }
+        if(position == 2){
+            holder.user_icon.setImageResource(R.drawable.member3)
+            holder.user_name.text = "ヤマダ"
+            holder.task_status_icon.setImageResource(R.drawable.study_status)
+            holder.remain_time.text  = "残り20分"
+            holder.task_name.text = "関連研究探し"
+        }
+    }
+
+    override fun getItemCount(): Int = 3
+
+    interface Listener{
+        fun onItemClicked(index: Int)
+    }
+}
