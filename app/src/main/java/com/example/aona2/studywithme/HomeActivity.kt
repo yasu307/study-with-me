a000b5029dc9034e1e7e7cb9669416026b3281db
yasu307 <yusuke307.ac@gmail.com>
2021-01-05 11:34:26 +0900

    add ログアウト機能

--- app/src/main/java/com/example/aona2/studywithme/HomeActivity.kt
+++ app/src/main/java/com/example/aona2/studywithme/HomeActivity.kt
@@ -3,7 +3,12 @@ package com.example.aona2.studywithme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(), StudyingFriendListAdapter.Listener {
@@ -21,6 +26,23 @@ class HomeActivity : AppCompatActivity(), StudyingFriendListAdapter.Listener {
             val intent = Intent(this, TaskNameInputActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_logout -> {
+                Firebase.auth.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     //recyclerViewのアイテムがクリックされた場合呼び出される
