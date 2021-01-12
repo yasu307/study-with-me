4cdc3bf85623916059b51f2e6d2fe4922ca139fa
yasu307 <yusuke307.ac@gmail.com>
2021-01-12 13:30:58 +0900

    時間管理の変数に軽い変更

--- app/src/main/java/com/example/aona2/studywithme/StudyActivity.kt
+++ app/src/main/java/com/example/aona2/studywithme/StudyActivity.kt
@@ -1,19 +1,26 @@
 package com.example.aona2.studywithme
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_study.*
-import java.time.Duration
-import java.time.LocalDateTime
+import java.text.SimpleDateFormat
+import java.util.*
 
 class StudyActivity : AppCompatActivity() {
-    private lateinit var startRoomAt: LocalDateTime
+    private var startRoomAtMillis: Long? = null
     private var myCountDownTimer: MyCountDownTimer? = null
+
+    //ログで時間を表示するときのフォーマット
+    private val simpleDateFormat = SimpleDateFormat("yyyy年MM月dd日 HH時mm分ss.SSS秒")
+
+    //一周の時間
+    private val oneRoopMin = 30
+    //休憩の時間
+    //つまり　勉強時間 = oneRoopMin - breaktimeMin
+    private val breaktimeMin = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +36,12 @@ class StudyActivity : AppCompatActivity() {
         val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         in_room_friend_recyclerview.addItemDecoration(itemDecoration)
 
-        //LocalDateTImeがオレオ以上でしか使えないので仕方なくバージョン確認
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ){
-            //テスト用にダミーのルーム開始時間を設定
-            startRoomAt = LocalDateTime.of(2021, 1, 2, 15, 13,0, 0)
-            Log.d("StudyActivity", "start room at $startRoomAt")
-        }
+        //ダミーのルーム開始時間をセット
+        val startRoomAtCalendar = Calendar.getInstance()
+        startRoomAtCalendar.set(2021,0,12,10,10,0)
+        startRoomAtMillis = startRoomAtCalendar.timeInMillis
+        Log.d("StudyActivity", "start room at  ${simpleDateFormat.format(startRoomAtMillis)}")
+        //Log.d("StudyActivity", "start room at millis is ${startRoomAtMillis} ")
 
         startTimer()
     }
@@ -42,27 +49,23 @@ class StudyActivity : AppCompatActivity() {
     //現在実行中のモードの残り時間を計算　返り値　Pair<残り時間（ミリ秒）,勉強中か？>
     //もっと簡素にできないか？
     //MyCountDownTImerに移行すべき？
-    private fun calcRemainTime() : Pair<Long, Boolean>{
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ) {
-            //現在の時間
-            val currentTime = LocalDateTime.now()
-            Log.d("StudyActivity", "current time is $currentTime ")
-            //経過時間
-            val elapsedTimeMillis = Duration.between(startRoomAt, currentTime ).toMillis()
-            Log.d("StudyActivity", "elapsed time millis is $elapsedTimeMillis")
-            //残り時間
-            val remainTimeMillis = ((30 * 60 * 1000) - (elapsedTimeMillis % (30 * 60 * 1000)))
-            Log.d("StudyActivity", "remain time is $remainTimeMillis")
+    private fun calcRemainTime(): Pair<Long, Boolean> {
+        if (startRoomAtMillis == null) return Pair(0,true)
 
-            //残り時間が5分以上なら勉強中、そうでないなら休憩中
-            if(remainTimeMillis >= 5 * 60 * 1000) return Pair(remainTimeMillis - 5 * 60 * 1000, true)
-            else return Pair(remainTimeMillis, false)
-        } else {
-            //バージョン確認がなくなったらここは削除する
-            //適当な値を返す
-            val remainTimeMillis = 0
-            return Pair(remainTimeMillis.toLong(), true)
-        }
+        //現在の時間
+        val currentTimeMillis = Calendar.getInstance().timeInMillis
+        Log.d("StudyActivity", "current time is ${simpleDateFormat.format(currentTimeMillis)} ")
+        //Log.d("StudyActivity", "current time millis is ${currentTimeMillis} ")
+        //経過時間
+        val elapsedTimeMillis = currentTimeMillis - startRoomAtMillis!!
+        Log.d("StudyActivity", "elapsed time millis is $elapsedTimeMillis")
+        //残り時間
+        val remainTimeMillis = ((oneRoopMin * 60 * 1000) - (elapsedTimeMillis % (oneRoopMin * 60 * 1000)))
+        Log.d("StudyActivity", "remain time is $remainTimeMillis")
+
+        //残り時間が5分以上なら勉強中、そうでないなら休憩中
+        if (remainTimeMillis >= breaktimeMin * 60 * 1000) return Pair(remainTimeMillis - breaktimeMin * 60 * 1000, true)
+        else return Pair(remainTimeMillis, false)
     }
 
     //タイマーの起動
