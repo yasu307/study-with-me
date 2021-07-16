08459e24e158f62547c3ceb19df36ca992fcedc5
yasu307 <yusuke307.ac@gmail.com>
2021-07-16 17:42:42 +0900

    今から一週間前の日のカレンダーを取得

--- app/src/main/java/com/example/aona2/studywithme/TimeManage/CalcStudyTime.kt
+++ app/src/main/java/com/example/aona2/studywithme/TimeManage/CalcStudyTime.kt
@@ -5,9 +5,9 @@ import com.example.aona2.studywithme.Model.StudyInfo
 import java.util.*
 
 class CalcStudyTime(val studyInfoList: MutableList<StudyInfo>) {
-    //一週間の勉強時間 (一昨日の勉強時間,昨日の勉強時間,今日の勉強時間)
+    //一週間の勉強時間 (...,一昨日の勉強時間,昨日の勉強時間,今日の勉強時間)
     var weeklyStudyTime = mutableListOf<Long>(0,0,0,0,0,0,0)
-    var now = Calendar.getInstance().timeInMillis
+    val now = Calendar.getInstance().timeInMillis
 
     companion object {
         //デバッグ時のコメント用
